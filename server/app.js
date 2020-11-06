35e8380da80984d04a2f20bdad46cec5911af446
daniel-livingston <danieljlivingston@protonmail.com>
2020-11-05 22:15:11 -0500

    Added favicon

--- server/app.js
+++ server/app.js
@@ -1,5 +1,6 @@
 const path = require("path");
 const express = require("express");
+const favicon = require("serve-favicon");
 const morgan = require("morgan");
 
 const app = express();
@@ -7,6 +8,7 @@ const app = express();
 app.use(express.static("public"));
 app.use(express.static("dist"));
 app.use(morgan("dev"));
+app.use(favicon(path.resolve(__dirname, "../public/favicon/favicon.ico")));
 
 app.get("*", (req, res) => {
 	res.sendFile(path.join(__dirname, "../public/index.html"));
